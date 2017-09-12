c0fab373626c75a1f78bf417369d02b27972307f
Adrian Krebs <krebs-adrian@bluewin.ch>
2017-09-12 19:23:20 +0200

    chart sample

--- src/app/pages/shared/diagrams/sentiments/sentiments.component.ts
+++ src/app/pages/shared/diagrams/sentiments/sentiments.component.ts
@@ -40,106 +40,168 @@ export class SentimentsComponent implements OnInit {
     const graphColor = this.baConfig.get().colors.custom.dashboardLineChart;
 
     return {
-      type: 'serial',
-      theme: 'blur',
-      marginTop: 15,
-      marginRight: 15,
-      responsive: {
-        'enabled': true
-      },
-      dataProvider: [
-        { date: new Date(2012, 11), value: 0, value0: 0 },
-        { date: new Date(2013, 0), value: 15000, value0: 19000},
-        { date: new Date(2013, 1), value: 30000, value0: 20000},
-
-
-        { date: new Date(2013, 2), value: 25000, value0: 22000},
-        { date: new Date(2013, 3), value: 21000, value0: 25000},
-        { date: new Date(2013, 4), value: 24000, value0: 29000},
-        { date: new Date(2013, 5), value: 31000, value0: 26000},
-        { date: new Date(2013, 6), value: 40000, value0: 25000},
-        { date: new Date(2013, 7), value: 37000, value0: 20000},
-        { date: new Date(2013, 8), value: 18000, value0: 22000},
-        { date: new Date(2013, 9), value: 5000, value0: 26000},
-        { date: new Date(2013, 10), value: 40000, value0: 30000},
-        { date: new Date(2013, 11), value: 20000, value0: 25000},
-        { date: new Date(2014, 0), value: 5000, value0: 13000},
-
-        { date: new Date(2014, 1), value: 3000, value0: 13000},
-        { date: new Date(2014, 2), value: 1800, value0: 13000},
-        { date: new Date(2014, 3), value: 10400, value0: 13000},
-        { date: new Date(2014, 4), value: 25500, value0: 13000},
-        { date: new Date(2014, 5), value: 2100, value0: 13000},
-        { date: new Date(2014, 6), value: 6500, value0: 13000},
-        { date: new Date(2014, 7), value: 1100, value0: 13000},
-        { date: new Date(2014, 8), value: 17200, value0: 13000},
-        { date: new Date(2014, 9), value: 26900, value0: 13000},
-        { date: new Date(2014, 10), value: 14100, value0: 13000},
-        { date: new Date(2014, 11), value: 35300, value0: 13000},
-        { date: new Date(2015, 0), value: 54800, value0: 13000},
-        { date: new Date(2015, 1), value: 49800, value0: 13000}
-      ],
-      categoryField: 'date',
-      categoryAxis: {
-        parseDates: true,
-        gridAlpha: 0,
-        color: layoutColors.defaultText,
-        axisColor: layoutColors.defaultText
-      },
-      valueAxes: [
-        {
-          minVerticalGap: 50,
-          gridAlpha: 0,
-          color: layoutColors.defaultText,
-          axisColor: layoutColors.defaultText
+      simpleLineOptions: {
+        color: this.baConfig.get().colors.defaultText,
+        fullWidth: true,
+        height: '300px',
+        chartPadding: {
+          right: 40
         }
-      ],
-      graphs: [
-        {
-          id: 'g0',
-          bullet: 'none',
-          useLineColorForBulletBorder: true,
-          lineColor: colorHelper.hexToRgbA(graphColor, 0.3),
-          lineThickness: 1,
-          negativeLineColor: layoutColors.danger,
-          type: 'smoothedLine',
-          valueField: 'value0',
-          fillAlphas: 1,
-          fillColorsField: 'lineColor'
+      },
+      simpleLineData: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        series: [
+          [20, 20, 12, 45, 50],
+          [10, 45, 30, 14, 12],
+          [34, 12, 12, 40, 50],
+          [10, 43, 25, 22, 16],
+          [3, 6, 30, 33, 43]
+        ]
+      },
+      areaLineData: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8],
+        series: [
+          [5, 9, 7, 8, 5, 3, 5, 4]
+        ]
+      },
+      areaLineOptions: {
+        fullWidth: true,
+        height: '300px',
+        low: 0,
+        showArea: true
+      },
+      biLineData: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        series: [
+          [1, 2, 3, 1, -2, 0, 1],
+          [-2, -1, -2, -1, -2.5, -1, -2],
+          [0, 0, 0, 1, 2, 2.5, 2],
+          [2.5, 2, 1, 0.5, 1, 0.5, -1]
+        ]
+      },
+
+      biLineOptions: {
+        height: '300px',
+        high: 3,
+        low: -3,
+        showArea: true,
+        showLine: false,
+        showPoint: false,
+        fullWidth: true,
+        axisX: {
+          showGrid: false
+        }
+      },
+      simpleBarData: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        series: [
+          [15, 24, 43, 27, 5, 10, 23, 44, 68, 50, 26, 8],
+          [13, 22, 49, 22, 4, 6, 24, 46, 57, 48, 22, 4]
+        ]
+      },
+      simpleBarOptions: {
+        fullWidth: true,
+        height: '300px'
+      },
+      multiBarData: {
+        labels: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+        series: [
+          [5, 4, 3, 7],
+          [3, 2, 9, 5],
+          [1, 5, 8, 4],
+          [2, 3, 4, 6],
+          [4, 1, 2, 1]
+        ]
+      },
+      multiBarOptions: {
+        fullWidth: true,
+        height: '300px',
+        stackBars: true,
+        axisX: {
+          labelInterpolationFnc: function (value) {
+            return value.split(/\s+/).map(function (word) {
+              return word[0];
+            }).join('');
+          }
         },
-        {
-          id: 'g1',
-          bullet: 'none',
-          useLineColorForBulletBorder: true,
-          lineColor: colorHelper.hexToRgbA(graphColor, 0.15),
-          lineThickness: 1,
-          negativeLineColor: layoutColors.danger,
-          type: 'smoothedLine',
-          valueField: 'value',
-          fillAlphas: 1,
-          fillColorsField: 'lineColor'
+        axisY: {
+          offset: 20
         }
+      },
+      multiBarResponsive: [
+        ['screen and (min-width: 400px)', {
+          reverseData: true,
+          horizontalBars: true,
+          axisX: {
+            labelInterpolationFnc: (n) => n
+          },
+          axisY: {
+            offset: 60
+          }
+        }],
+        ['screen and (min-width: 700px)', {
+          stackBars: false,
+          reverseData: false,
+          horizontalBars: false,
+          seriesBarDistance: 15
+        }]
       ],
-      chartCursor: {
-        categoryBalloonDateFormat: 'MM YYYY',
-        categoryBalloonColor: '#4285F4',
-        categoryBalloonAlpha: 0.7,
-        cursorAlpha: 0,
-        valueLineEnabled: true,
-        valueLineBalloonEnabled: true,
-        valueLineAlpha: 0.5
+      stackedBarData: {
+        labels: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+        series: [
+          [800000, 1200000, 1400000, 1300000],
+          [200000, 400000, 500000, 300000],
+          [100000, 200000, 400000, 600000]
+        ]
       },
-      dataDateFormat: 'MM YYYY',
-      export: {
-        enabled: true
+      stackedBarOptions: {
+        fullWidth: true,
+        height: '300px',
+        stackBars: true,
+        axisY: {
+          labelInterpolationFnc: function (value) {
+            return (value / 1000) + 'k';
+          }
+        }
       },
-      creditsPosition: 'bottom-right',
-      zoomOutButton: {
-        backgroundColor: '#fff',
-        backgroundAlpha: 0
+      simplePieData: {
+        series: [5, 3, 4]
       },
-      zoomOutText: '',
-      pathToImages: layoutPaths.images.amChart
+      simplePieOptions: {
+        fullWidth: true,
+        height: '300px',
+        weight: '300px',
+        labelInterpolationFnc: function (value) {
+          return Math.round(value / 12 * 100) + '%';
+        }
+      },
+      labelsPieData: {
+        labels: ['Bananas', 'Apples', 'Grapes'],
+        series: [20, 15, 40]
+      },
+      labelsPieOptions: {
+        fullWidth: true,
+        height: '300px',
+        weight: '300px',
+        labelDirection: 'explode',
+        labelInterpolationFnc: function (value) {
+          return value[0];
+        }
+      },
+      simpleDonutData: {
+        labels: ['Bananas', 'Apples', 'Grapes'],
+        series: [20, 15, 40]
+      },
+      simpleDonutOptions: {
+        fullWidth: true,
+        donut: true,
+        height: '300px',
+        weight: '300px',
+        labelDirection: 'explode',
+        labelInterpolationFnc: function (value) {
+          return value[0];
+        }
+      }
     };
   }
 
