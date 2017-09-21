99233057828c0d05e957409b32fb643f75e94ab4
kow <werner.koch@bluewin.ch>
2017-09-21 11:24:05 +0200

    (kow) Diagramme in Parlament, Partei und Politiker eingebaut.

---
+++ src/app/pages/shared/diagrams/queryHelper.ts
@@ -0,0 +1,15 @@
+import {QueryBuilder} from "../services/queryBuilder";
+
+export class QueryHelper {
+  private queryBuilder: QueryBuilder = new QueryBuilder();
+
+  createQueryString(party: string, politicianId: string) {
+    if (party) {
+      return this.queryBuilder.queryFromPartyName(party);
+    } else if (politicianId) {
+      return this.queryBuilder.queryFromPoliticianId(politicianId);
+    } else {
+      return '';
+    }
+  }
+}
