f689888e016299419fd94965fd4a843a381811f2
kow <werner.koch@bluewin.ch>
2017-09-26 17:09:36 +0200

    (kow) Kleine Fixes

--- src/app/pages/shared/shared.module.ts
+++ src/app/pages/shared/shared.module.ts
@@ -1,19 +1,19 @@
-import {NgModule}            from '@angular/core';
-import {CommonModule}        from '@angular/common';
-import {FormsModule}         from '@angular/forms';
-import {Tile} from "./tile/tile.component";
-import {AppTranslationModule} from "../../app.translation.module";
-import {NgaModule} from "../../theme/nga.module";
-import {Helper} from "app/pages/util/helper.service";
-import {TileService} from "./tile/tile.service";
-import {ParlamentService} from "./services/paralament.service";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tile } from './tile/tile.component';
+import { AppTranslationModule } from '../../app.translation.module';
+import { NgaModule } from '../../theme/nga.module';
+import { Helper } from 'app/pages/util/helper.service';
+import { TileService } from './tile/tile.service';
+import { ParlamentService } from './services/paralament.service';
 import { SentimentsComponent } from './diagrams/sentiments/sentiments.component';
 import { MentionsComponent } from './diagrams/mentions/mentions.component';
 import { MentionsService } from './services/mentions.service';
 import { SentimentsService } from './services/sentiments.service';
 import { SentimentsTransformerService } from './diagrams/sentiments/sentiments-transformer.service';
 import { MentionsTransformerService } from './diagrams/mentions/mentions-transformer.service';
-import {Feed } from './feed/feed.component';
+import { Feed } from './feed/feed.component';
 
 
 @NgModule({
