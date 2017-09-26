699ef23c07ce96a96f212a9adb399fa9780d0139
christophgwerder <christoph.gwerder@insel.ch>
2017-09-26 18:44:02 +0200

    Bundesversammlung mit Twitter-Aktivität pro Politiker

--- src/app/pages/parliament/federal-assembly/federal-assembly.component.ts
+++ src/app/pages/parliament/federal-assembly/federal-assembly.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { MAPPING } from '../../util/mapping';
+
 import { SeatModel } from './service/federal-assembly.model';
 import { FederalAssemblyService } from './service/federal-assembly.service';
+
+import { Helper } from '../../util/helper.service';
+import { ParlamentService } from '../../shared/services/paralament.service';
+
+import { TileService } from '../../shared/tile/tile.service';
 
 @Component({
   selector: 'app-federal-assembly',
@@ -13,35 +19,113 @@ import { FederalAssemblyService } from './service/federal-assembly.service';
 })
 
 export class FederalAssemblyComponent implements OnInit {
- 
-  seats;
+
+  // private tweetsToday: Number;
+  private seats;
+  private usersToday;
+  private selectedSeat: SeatModel;
+
 
   getSeats(): void {
     this.seats = this.federalAssemblyService.getSeats();
   }
 
-  constructor(private federalAssemblyService: FederalAssemblyService, 
-              private route: ActivatedRoute, private router: Router) { }
+  updateSeats() {
+    //this.seats
+    //getTweetsToday
+  }
+
+  getTwitterData(politicianId) {
+    let queryParmas = '';
+    if (politicianId) {
+      queryParmas = '?politicianId=' + politicianId;
+      this.tileService.getTweetsToday(queryParmas).subscribe((data) => {
+        // console.log(data);
+        return data.tweets;
+      });
+    }
+  }
+
+
+  getIndexForPersonId(id) {
+    return this.helperService.getIndexForPersonId(id);
+  }
+
+  getPoliticanInfos(id) {
+    return this.parlamentService.getPoliticanInfos(id);
+  }
+
+  constructor(private federalAssemblyService: FederalAssemblyService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private helperService: Helper,
+    private parlamentService: ParlamentService,
+    private tileService: TileService) { }
 
   ngOnInit(): void {
+
     this.getSeats();
-    this.seats.forEach( seat => {
-      
-      seat.numberOfTweets = (Math.random() * 20 );
-      
-      if (seat.numberOfTweets < 1) {
-        seat.colorCode = 'grey';
-      } else if (seat.numberOfTweets < 5) {
-        seat.colorCode = 'lightgreen';
-      } else if (seat.numberOfTweets < 10) {
-        seat.colorCode = 'green';
+    // TODO
+    // this.getTweetsPerPolitician();
+    this.seats.forEach(seat => {
+
+      seat.mappingIndex = this.getIndexForPersonId(seat.personId);
+
+      if (seat.mappingIndex !== -1) {
+        seat.party = MAPPING[seat.mappingIndex].party;
+        seat.twitterId = MAPPING[seat.mappingIndex].id;
+      }
+
+      seat.numberOfTweets = 0;
+      // seat.numberOfTweets = this.getTwitterData(seat.personId);
+
+      /*
+      if (seat.twitterId !== undefined) {
+        let queryParmas = '';
+        // queryParmas = '?politicianId=' + seat.personId;
+        this.tileService.getTweetsToday(queryParmas).subscribe((data) => {
+          // console.log(data);
+          // seat.numberOfTweets = data;
+          seat.numberOfTweets = data.tweets + (Math.random() * 20);
+          this.seats = Array.from(this.seats);
+        });
+      }
+      */
+
+      // TODO
+      // Anzahl Tweets pro Tag und Politiker einfügen
+      seat.numberOfTweets = (Math.random() * 20);
+
+      if (seat.twitterId !== undefined) {
+        seat.twitterClass = 'has-no-twitter-account';
       } else {
-        seat.colorCode = 'darkgreen';
+        seat.twitterClass = 'has-twitter-account';
+      }
+
+      if (seat.numberOfTweets < 1) {
+        seat.activityClass = 'no-activity';
+      } else if (seat.numberOfTweets < 5) {
+        seat.activityClass = 'low-activity';
+      } else if (seat.numberOfTweets < 10) {
+        seat.activityClass = 'medium-activity';
+      } else {
+        seat.activityClass = 'high-activity';
       }
     });
   }
 
+
   navigateToProfile(personId) {
     this.router.navigate(['/pages/politician/' + personId]);
   }
+
+  showPreview(personId) {
+    this.router.navigate(['/pages/politician/' + personId]);
+  }
+
+  setSelectedSeat(seat) {
+    // console.log(seat);
+    this.selectedSeat = seat;
+  }
+
 }
